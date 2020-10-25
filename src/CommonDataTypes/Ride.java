e3b0f8f2ed29166d444e74ef1f24ef2c194e33d9
Swistusmen <michal76aw@gmail.com>
2020-10-25 15:32:53 +0100

    Adding serialization mechanism to communicators

--- src/CommonDataTypes/Ride.java
+++ src/CommonDataTypes/Ride.java
@@ -1,5 +1,20 @@
 package CommonDataTypes;
 
-public enum Ride {
+import java.io.Serializable;
 
+public class Ride implements Serializable {
+    public String inputAddress;
+    public String outputAddress;
+    public String carNumber;
+    public String phoneDriver;
+    public String phoneClient;
+    public RideState state=RideState.Unordered;
+    public float price;
+
+    public String printRide()
+    {
+        String text=inputAddress+" "+outputAddress+" "+carNumber+" "+phoneDriver+" "+phoneClient+" "+state.toString()+" "+price;
+        return text;
+    }
 }
+
