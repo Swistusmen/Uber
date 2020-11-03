cdb43aa11741bd96d9d3c3fb1adb73b5909a253b
Rafał <56602528+rwidzisz@users.noreply.github.com>
2020-11-03 14:03:27 +0100

    Update DriverOperations.java

--- src/DriverMobileApplication/DriverOperations.java
+++ src/DriverMobileApplication/DriverOperations.java
@@ -1,4 +1,9 @@
 package DriverMobileApplication;
 
 public interface DriverOperations {
+  Ride LookForAClient();
+    float CheckWallet();
+    boolean AcceptARide();
+    boolean ReportDriveHasEnded();
+    boolean ReportAProblem();  
 }
