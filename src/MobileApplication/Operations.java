d93eb014b776f600bce0b53d517e06b5ba689905
Rafał <56602528+rwidzisz@users.noreply.github.com>
2020-11-03 14:04:40 +0100

    Update Operations.java

--- src/MobileApplication/Operations.java
+++ src/MobileApplication/Operations.java
@@ -1,4 +1,10 @@
 package MobileApplication;
 
 public interface Operations {
+    boolean Rate();
+    boolean SignUp();
+    boolean SignIn();
+    boolean ReportAProblem();
+    boolean CancelARide();
+    void ShowARide();
 }
