d021f8f4ac96518cddd42ad73fe76bebc587b9a2
Rafał <56602528+rwidzisz@users.noreply.github.com>
2020-12-26 20:51:37 +0100

    Add files via upload

---
+++ src/GUIDriver/DControllerRide.java
@@ -0,0 +1,17 @@
+package GUIDriver;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class DControllerRide {
+
+    public void CancelRideButton(ActionEvent event) throws IOException {
+
+        //cancel ride operations
+        //money operations
+
+        System.exit(1);
+
+    }
+}
